bc0ad47d22aa39a0bb495c1c16371c203a998d8a
MalickaHoumgbo <malickamaeva.houmgbombakop@epfedu.fr>
2026-08-23 14:10:48 -0700

    docs: verifier_la_granularité_des colonnes
    Les colonnes concernées sont:
    unit_cost/unit_price/reorder_point/supplier_lead_time_days

--- sql/02_granularity_check_fact_attributes..sql
+++ sql/02_granularity_check_fact_attributes..sql
@@ -11,3 +11,9 @@ select sku_id,
 from `logidistrib_dwh.stg_inventory_raw`
 group by sku_id, warehouse_id
 having count(distinct unit_price) > 1  
+
+select sku_id, 
+       COUNT(DISTINCT reorder_point),
+       count(distinct supplier_lead_time_days)
+from `logidistrib_dwh.stg_inventory_raw`
+group by sku_id
